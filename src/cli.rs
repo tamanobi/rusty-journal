f662bd16199f09ff7710af1e98224945bbae7274
tamanobi <tamanobi@gmail.com>
2021-04-03 23:59:22 +0900

    feat: cli

---
+++ src/cli.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    Add {
+        /// description
+        #[structopt()]
+        task: String
+    },
+    Done {
+        /// position
+        #[structopt()]
+        position: usize
+    },
+    /// List all
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Rusty Journal",
+    about = "Journal"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
