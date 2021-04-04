c9e604491227791a0684c7c2d98c2925833e1963
tamanobi <tamanobi@gmail.com>
2021-04-04 14:19:59 +0900

    feat: use struct

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,8 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+use chrono::{serde::ts_seconds, DateTime, Utc, Local};
+use serde::Deserialize;
+use serde::Serialize;
 
 #[derive(Debug, StructOpt)]
 pub enum Action {
@@ -27,4 +30,18 @@ pub struct CommandLineArgs {
     pub action: Action,
     #[structopt(parse(from_os_str), short, long)]
     pub journal_file: Option<PathBuf>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task {
+    pub text: String,
+    #[serde(with = "ts_seconds")]
+    pub created_at: DateTime<Utc>,
+}
+
+impl Task {
+    pub fn new(text: String) -> Task {
+        let created_at: DateTime<Utc> = Utc::now();
+        Task { text, created_at }
+    }
 }
