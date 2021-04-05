bf1ab0393763eb93239f48912308b8e282748fa6
tamanobi <tamanobi@gmail.com>
2021-04-06 03:09:53 +0900

    refactor: Task

--- src/task.rs
+++ src/task.rs
@@ -1,5 +1,22 @@
 use std::fs::OpenOptions;
 use std::io::{BufReader, Result, Seek, SeekFrom};
+use chrono::{serde::ts_seconds, DateTime, Utc, Local};
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task {
+    pub text: String,
+    #[serde(with = "ts_seconds")]
+    pub created_at: DateTime<Utc>,
+}
+
+impl Task {
+    pub fn new(text: String) -> Task {
+        let created_at: DateTime<Utc> = Utc::now();
+        Task { text, created_at }
+    }
+}
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     let file = OpenOptions::new()
