51b50b197912c52aa99db755addd2890551c6bdd
tamanobi <tamanobi@gmail.com>
2021-04-06 10:08:15 +0900

    feat: it works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-mod cli;
 use structopt::StructOpt;
+mod cli;
+mod tasks;
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
 
 fn main() {
-    let args = cli::CommandLineArgs::from_args();
-    dbg!(&args);
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+    // dbg!(&args);
+
+    let journal_path = journal_file.expect("Failed to find journal file");
+    match action {
+        Add { task } => tasks::add_task(journal_path, Task::new(task)),
+        List => tasks::list_tasks(journal_path),
+        Done { position } => tasks::complete_task(journal_path, position),
+    }
+    .expect("Failed to perform action")
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,9 +1,10 @@
-use std::fmt;
-use std::fs::OpenOptions;
-use std::io::{BufReader, Result, Seek, SeekFrom, Error, ErrorKind};
-use chrono::{serde::ts_seconds, DateTime, Utc, Local};
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::fmt;
+use std::fs::{File, OpenOptions};
+use std::io::{BufReader, Error, ErrorKind, Result, Seek, SeekFrom};
+use std::path::PathBuf;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -19,7 +20,6 @@ impl Task {
     }
 }
 
-
 impl fmt::Display for Task {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
@@ -28,7 +28,7 @@ impl fmt::Display for Task {
 }
 
 fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
-    let tasks: Vec<Tasks> = match serde_json::from_reader(file) {
+    let tasks: Vec<Task> = match serde_json::from_reader(file) {
         Ok(tasks) => tasks,
         Err(e) if e.is_eof() => Vec::new(),
         Err(e) => Err(e)?,
@@ -53,17 +53,16 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     Ok(())
 }
 
-
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
-    let file = OpenOptions::new()
+    let mut file = OpenOptions::new()
         .read(true)
         .write(true)
-        .open(journal_path);
+        .open(journal_path)?;
     let mut tasks = collect_tasks(&file)?;
     if task_position == 0 || task_position > tasks.len() {
         return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
     }
-    tasks.remove(task_position);
+    tasks.remove(task_position - 1);
 
     file.seek(SeekFrom::Start(0))?;
     file.set_len(0);
