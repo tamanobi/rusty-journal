3a249643a2e1eef140c7f2d038360c55d72bc5d0
tamanobi <tamanobi@gmail.com>
2021-04-07 22:53:07 +0900

    feat: read journal file from home dir

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use structopt::StructOpt;
 mod cli;
 mod tasks;
 use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
 use tasks::Task;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
 
 fn main() {
     let CommandLineArgs {
@@ -11,7 +19,9 @@ fn main() {
     } = CommandLineArgs::from_args();
     // dbg!(&args);
 
-    let journal_path = journal_file.expect("Failed to find journal file");
+    let journal_path = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file");
     match action {
         Add { task } => tasks::add_task(journal_path, Task::new(task)),
         List => tasks::list_tasks(journal_path),
